340bcf2361af54ec55b6b43e70510705b28e8220
Matt Zhou <mattzmyname@gmail.com>
2019-11-14 23:28:55 -0500

    set up project to work in src

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Blackjack_game from "./bj4d/Blackjack_game";
+
+function App() {
+  return (
+    <div className="App">
+      <Blackjack_game></Blackjack_game>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/bj4d/Blackjack_game.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './Blackjack_game.css';
+
+export default class Blackjack_game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        foobar
+      </div>
+    );
+  }
+}
