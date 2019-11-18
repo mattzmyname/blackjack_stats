6276784ce56bbd67c40d187420f199c2efdc97c8
Matt Zhou <mattzmyname@gmail.com>
2019-11-17 22:46:28 -0500

    added assets

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,34 +11,15 @@ import React, {Component} from 'react';
  * @return     {ReactElement}    markup
  */
 export default class Card extends Component{
-    renderContainer = () => {
-        return (
-            <div className="container">
-                <span className="rank">{this.props.rank}</span>
-                <span className="suit">{this.props.suit}</span>
-            </div>
-        );
-    };
-    renderFront = () => {
-        return (
-            <div className="front">
-                <div className="section top">
-                    {this.renderContainer()}
-                </div>
-                <div className="section center suit">{this.props.suit}</div>
-                <div className="section bottom">
-                    {this.renderContainer()}
-                </div>
-            </div>
-        );
-    };
+
     render() {
-        console.log(this.props)
+        let bgUrl = (this.props.isPrivate)
+            ? 'url(assets/hidden.png)'
+            : 'url(assets/' + this.props.suit + + this.props.rank + '.png)';
+        /* in react we pass the css properties as an object with camelCase variables referring to the respective CSS variables */
+        let cardStyle = {backgroundImage: bgUrl};
         return(
-            <div className={`card ${this.props.suit}`} data-private={this.props.isPrivate}>
-                {!this.props.isPrivate && this.renderFront()}
-                <div className="back">{this.props.isPrivate ? "Back" : ""}</div>
-            </div>
+            <div className='card' style={cardStyle}/>
         )
     }
 
--- src/components/Hand.js
+++ src/components/Hand.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import Card from './Card';
 
 export default class Table extends Component{
+    constructor(props){
+        super(props)
+    }
+
     render (){
         return(
             <div>
