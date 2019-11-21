8579b25b2734a711f1f7ab7bec90492960a72a1f
mattzmyname <mwz62905@uga.edu>
2019-11-21 00:41:35 -0500

    work on betting feature

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import {getWinner} from "../logic/functions";
 import Button from 'react-bootstrap/Button';
-import Dropdown from 'react-bootstrap/Dropdown'
-import DropdownButton from "react-bootstrap/DropdownButton";
+
 
 
 export default class Interface extends Component{
@@ -40,11 +39,7 @@ export default class Interface extends Component{
                 <Button className="m-1" color="outline-info" size="lg" onClick={!this.props.activeGame ? this.props.dealButton : null}>Deal</Button>
                 <Button className="m-1" color="success" size="lg" onClick={this.props.activeGame ? this.props.hitButton : null}>Hit</Button>
                 <Button className="m-1" color="danger" size="lg" onClick={this.props.activeGame ? this.props.standButton : null}>Stand</Button>
-                <DropdownButton id={"dropdown-basic-button"} title={"Choose Your Bet"} onChange={(e) => this.betInput(e)}>
-                    <Dropdown.Item value={10}>$10</Dropdown.Item>
-                    <Dropdown.Item value={20}>$20</Dropdown.Item>
-                    <Dropdown.Item value={30}>$30</Dropdown.Item>
-                </DropdownButton>
+
             </div>
         )
     }
