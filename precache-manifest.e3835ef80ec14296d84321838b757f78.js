7ceba01f2af79a69f13457ee3e471c2c50803902
mattzmyname <mwz62905@uga.edu>
2019-12-04 02:31:38 -0500

    Updates

---
+++ precache-manifest.e3835ef80ec14296d84321838b757f78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103dbed725e808d13d96acb4e7c7289b",
+    "url": "/blackjack_stats/index.html"
+  },
+  {
+    "revision": "f96a9b163369b281cde0",
+    "url": "/blackjack_stats/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "1ca85ce17d33c9b4317a",
+    "url": "/blackjack_stats/static/css/main.f3dd5008.chunk.css"
+  },
+  {
+    "revision": "f96a9b163369b281cde0",
+    "url": "/blackjack_stats/static/js/2.65d3e420.chunk.js"
+  },
+  {
+    "revision": "1ca85ce17d33c9b4317a",
+    "url": "/blackjack_stats/static/js/main.de899e65.chunk.js"
+  },
+  {
+    "revision": "40125ecdbb07d2f699d9",
+    "url": "/blackjack_stats/static/js/runtime-main.e0e06b11.js"
+  },
+  {
+    "revision": "922d80addbb4394907d7f526ebcc7624",
+    "url": "/blackjack_stats/static/media/back.922d80ad.png"
+  },
+  {
+    "revision": "6c6ed6d19f38a1dad7fe3ddefe379f63",
+    "url": "/blackjack_stats/static/media/back@2X.6c6ed6d1.png"
+  }
+]);
